3935fb56dca50cfc3c059742849cd62933c3d8a7
Jean <jeanncavalcante@gmail.com>
2023-10-28 19:42:11 -0300

    feat: iniciando projeto com tamagui

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,16 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Required for expo-router
       'expo-router/babel',
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+        },
+      ],
+      "react-native-reanimated/plugin"
     ],
   };
 };
